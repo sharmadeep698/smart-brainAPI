6624deb2a1906658f98f4b68d25c1b6f44650c85
sharmadeep698 <64363262+sharmadeep698@users.noreply.github.com>
2020-05-29 11:44:06 +0545

    adding port

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res) => {image.handleApi(req,res)})
 
-app.listen(3000,()=>{
-	console.log('helllooooo');
+app.listen(process.env.PORT || 3000,()=>{
+	console.log(`app is running on ${process.env.PORT}`);
 }) 
